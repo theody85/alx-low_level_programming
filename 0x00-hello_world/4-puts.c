7d1fc4c85debb54b09704b94bc30c07b67cbad62
theody85 <gyambrahtheodora.8@gmail.com>
2022-03-10 20:10:37 +0000

    Add 4-puts.c C program to print using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 	char str[] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
-	
-	return(0);
+puts(str);
+return (0);
 }
