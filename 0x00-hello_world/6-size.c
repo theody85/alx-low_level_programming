7b1361b0e0219121838a87637df6b51787b73677
theody85 <gyambrahtheodora.8@gmail.com>
2022-03-10 22:06:23 +0000

    Add 6-size.c script to print the size of various datatypes on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char myChar;
+int myInt;
+long int myLongInt;
+long long int myLongLongInt;
+float myFloat;
+
+printf("Size of a char: %zu byte(s)\n", sizeof(myChar));
+printf("Size of a int: %zu byte(s)\n", sizeof(myInt));
+printf("Size of a long int: %zu byte(s)\n", sizeof(myLongInt));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(myLongLongInt));
+printf("Size of a float: %zu byte(s)\n", sizeof(myFloat));
+return (0);
+}
