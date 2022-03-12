b9e643f38b28d9fbef213f3acfbd3c519dc1d514
theody85 <gyambrahtheodora.8@gmail.com>
2022-03-12 00:27:59 +0000

    Add 6-print_numberz.c to print decimal digits using putchar from a non-char declared variable

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
