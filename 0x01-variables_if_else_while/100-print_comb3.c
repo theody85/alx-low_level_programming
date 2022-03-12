5957500c267b90bf1e47e1de962b891a26e0624e
theody85 <gyambrahtheodora.8@gmail.com>
2022-03-12 02:35:51 +0000

    Add 100-print_com3 to print combination of two numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n = 0, s;
+
+	while (n <= 9)
+	{
+		for (s = 0; s <= 9; s++)
+		{
+			if (n < s)
+			{
+				putchar(n + '0');
+				putchar(s + '0');
+				
+				if (n != 8 || (n == 8 && s != 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
