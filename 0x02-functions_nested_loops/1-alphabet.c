09c5261d386db7b3756aa7b19dde52568ae16232
theody85 <gyambrahtheodora.8@gmail.com>
2022-03-15 17:55:23 +0000

    Add files for task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,4 @@
-# include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+# include "main.h"
 
 /**
  * print_alphabet - Prints the alphabets in lower case
@@ -24,10 +11,10 @@ void print_alphabet(void)
 
 	while (a <= z)
 	{
-		putchar(a);
+		_putchar(a);
 		a++;
 	}
 
-	putchar('\n');
+	_putchar('\n');
 
 }
