39de3a2584891c5153aea6390db69ca448ad7e78
theody85 <gyambrahtheodora.8@gmail.com>
2022-03-15 17:45:42 +0000

    Add files for task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-# include <stdio.h>
+# include "main.h"
 
 /**
  * main - Prints _putchar, followed by a new line.
@@ -8,9 +8,15 @@
 
 int main(void)
 {
-	char *str = "_putchar";
-
-	puts(str);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
