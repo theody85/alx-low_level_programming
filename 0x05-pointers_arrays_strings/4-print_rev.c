4783a6ffd5a18b13b01b315417db7f4ced05797f
theody85 <gyambrahtheodora.8@gmail.com>
2022-03-22 00:21:52 +0000

    Add files for task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in revers, followed by a new line, to stdout.
+ * @*str: the first character of the string
+ * @str: address of the first character of the string
+ *
+ * Return: none
+ */
+void print_rev(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+		i++;
+
+	while (i >= 0)
+	{
+		_putchar(*(str + i));
+		i--;
+	}
+	_putchar('\n');
+}
