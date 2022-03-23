54413ece665c787f7801ce6ac776b52e9e93d3a3
theody85 <gyambrahtheodora.8@gmail.com>
2022-03-23 23:21:12 +0000

    Update README.md

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -22,11 +22,12 @@ void puts_half(char *str)
 		{
 			_putchar(str[i]);
 		}
-	} else if (len % 2)
+	} 
+	else if (len % 2)
 	{
-		for (n = (len - 1) / 2; n < len - 1; n++)
+		for (n = (len - 1) / 2; n < len ; n++)
 		{
-			_putchar(str[n + 1]);
+			_putchar(str[n]);
 		}
 	}
 	_putchar('\n');
