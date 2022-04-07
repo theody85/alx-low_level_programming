1ff4102959b98244fa0a5f29cf99aae13537e45a
theody85 <gyambrahtheodora.8@gmail.com>
2022-04-07 18:59:03 +0000

    Add files

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,14 +2,16 @@
 
 /**
  * _calloc - allocates memory for an array using malloc
- * @nmeb: number of elements of array
+ * @nmemb: number of elements of array
  * @size: size of bytes of elements
+ *
+ * Return: a pointer to the allocated space
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arr;
-	
-	if(nmemb == 0 || size == 0)
+
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	arr = malloc(nmemb * size);
